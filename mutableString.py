b3795772ec922bf552ea4b1699988b8b67244c54
kong-fan-xing123 <kfstary@outlook.com>
2021-08-18 10:18:34 +0800

    Update mutableString.py

--- mutableString.py
+++ mutableString.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3.10
+
 class String():
 	def __init__(self, object: str=''):
 		self.__listofthestring = list(object)
 	def __getitem__(self,index):
-		return self.__listofthestring[index]
+		try:
+			return self.__listofthestring[index]
+		except BaseException as ncerr:
+			raise IndexError('string index out of range') from ncerr
 	def __setitem__(self,index,value):
-		self.__listofthestring[index] = value
+		try:
+			self.__listofthestring[index]=value
+		except BaseException as ncerr:
+			raise IndexError('string index out of range') from ncerr
 	def __str__(self) -> str:
 		return ''.join(self.__listofthestring)
 	def __repr__(self) -> str:
@@ -18,13 +25,13 @@ class String():
 		return self.__class__(''.join(self.__listofthestring) + str(o))
 	def __radd__(self,o):
 		return self.__class__(''.join(self.__listofthestring) + str(o))
-	def __sub__(self,o):
-		return self.__class__(''.join(self.__listofthestring) - str(o))
-	def __rsub__(self,o):
-		return self.__class__(''.join(self.__listofthestring) - str(o))
 	def format(self,*pargs,**kwargs):
 		res=''.join(self.__listofthestring)
-		res.format(*pargs,**kwargs)
+		return self.__class__(res.format(*pargs,**kwargs))
+	def __mul__(self,other):
+		return self.__class__(''.join(self.__listofthestring))
+	def __rmul__(self,other):
+		return self.__class__(''.join(self.__listofthestring))
 
 
 
